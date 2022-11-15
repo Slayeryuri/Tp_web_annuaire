7746b8cbb62c0a44c024ccad5acc1ae174abbcf7
Slayeryuri <nicolas.lagarde16@gmail.com>
2022-11-15 17:30:05 +0100

    Premier commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <router-link to="/Personnes" class="navbar-brand">Web Annuaire</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/Personnes" class="nav-link">Personnes</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/ajouter-personne" class="nav-link">Ajouter</router-link>
+        </li>
+      </div>
+    </nav>
+
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
---
+++ src/components/Personne.vue
@@ -0,0 +1,116 @@
+<template>
+
+  <div v-if="currentPersonne">
+    
+    {{ currentPersonne.id }}
+    {{ currentPersonne.name }}
+    {{ currentPersonne.surname }}
+    {{ currentPersonne.phone }}
+    {{ currentPersonne.city }}
+
+    <div class="form-group">
+       <label for="name"> Nom </label>  
+       <input type="text" class="form-control" id="name" v-model="currentPersonne.name">
+    </div>
+    <div class="form-group">
+       <label for="name"> Prénom </label>  
+       <input type="text" class="form-control" id="surname" v-model="currentPersonne.surname">
+    </div>
+    <div class="form-group">
+       <label for="name"> Téléphone </label>  
+       <input type="number" class="form-control" id="phone" v-model="currentPersonne.phone">
+    </div>
+    <div class="form-group">
+       <label for="name"> Ville </label>  
+       <input type="text" class="form-control" id="city" v-model="currentPersonne.city">
+    </div>
+
+<!-- <div class="form-group">
+       <label for="name"> Ville </label>  
+       <input type="text" class="form-control" id="city" v-model="currentPersonne.city">
+     </div>
+-->
+
+
+  </div>
+  
+
+
+    <!-- A INCLURE DANS LE FORM -->
+    <button class="badge badge-danger mr-2"
+      @click="deletePersonne">
+      Supprimer
+    </button>
+
+    <!-- A INCLURE DANS LE FORM -->
+    <button type="submit" class="badge badge-success"
+      @click="updatePersonne">
+      Modifier
+    </button>
+    <p>{{ message }}</p>
+
+
+    
+</template>
+
+<script>
+import PersonneDataService from "../services/PersonneDataService";
+
+export default {
+  name: "personne",
+  data() {
+    return {
+      currentPersonne: null,
+      message: ''
+    };
+  },
+  methods: {
+    getPersonne(id) {
+       PersonneDataService.get(id)
+        .then(response => {
+          this.currentPersonne = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+      
+    },
+
+    updatePersonne() {
+      PersonneDataService.update(this.currentPersonne)
+      .then(response => {
+          this.currentPersonne = response.data;
+          console.log(response.data);
+          this.message = 'Personne modifiée avec succès!';
+
+        })
+        .catch(e => {console.log(e);}
+          );
+    },
+
+    deletePersonne() {
+      PersonneDataService.delete(this.currentPersonne.id)
+        .then(response => {
+          this.currentPersonne = response.data;
+          console.log(response.data);
+          this.$router.push({ name:"personnes" });
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  },
+  mounted() {
+    this.message = '';
+    this.getPersonne(this.$route.params.id);
+  }
+};
+</script>
+
+<style>
+.edit-form {
+  max-width: 300px;
+  margin: auto;
+}
+</style>
